5b31fd24cb4f2c72ad401e59d3097d0a3ed13b7c
Kasper Tontti <kaspertontti@Kaspers-MacBook-Pro.local>
2017-11-01 18:23:55 +0200

    CSS Layoutti

--- src/map-settings.js
+++ src/map-settings.js
@@ -1,9 +1,9 @@
-import { geoMercator, geoPath } from 'd3-geo';
+import { geoNaturalEarth1, geoPath } from 'd3-geo';
 import { select } from 'd3-selection';
 
 export const mapWidth = 950;
 export const mapHeight = 700;
-export const projection = geoMercator().scale(150).translate([mapWidth / 2, mapHeight / 1.5]);
+export const projection = geoNaturalEarth1().scale(200).translate([mapWidth / 2, mapHeight / 1.5]);
 export const path = geoPath().pointRadius(2).projection(projection);
 export const svg = select('#map')
     .append('svg')
