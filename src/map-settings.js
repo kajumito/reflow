7d5bcb1ac2b52c62f8f125acd88fe7b20e790c1e
Kasper Tontti <kaspertontti@Kaspers-MacBook-Pro.local>
2017-11-04 11:38:13 +0200

    UI layout -valmis

--- src/map-settings.js
+++ src/map-settings.js
@@ -1,9 +1,9 @@
 import { geoNaturalEarth1, geoPath } from 'd3-geo';
 import { select } from 'd3-selection';
 
-export const mapWidth = 950;
-export const mapHeight = 700;
-export const projection = geoNaturalEarth1().scale(200).translate([mapWidth / 2, mapHeight / 1.5]);
+export const mapWidth = 1200;
+export const mapHeight = 600;
+export const projection = geoNaturalEarth1().scale(225).translate([mapWidth / 2.25, mapHeight / 1.75]);
 export const path = geoPath().pointRadius(2).projection(projection);
 export const svg = select('#map')
     .append('svg')
