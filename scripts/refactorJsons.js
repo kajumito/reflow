4babdbd46c8d0188f98ee1611b9b104d5caa0e1d
Mikko Kokkoniemi <mikko.kokkoniemi@meiko.fi>
2017-11-20 12:00:13 +0200

    faster webpack build… better uglify for production … improved json refactor script

--- scripts/refactorJsons.js
+++ scripts/refactorJsons.js
@@ -1,20 +1,26 @@
 /**
- * This script replaces occurances of country names to matching
- * name in dict from all json files in src/data-folder
+ * This script replaces all occurances of country names to matching
+ * names in dict in all json files in src/data -folder
  */
 const fs = require('fs');
 const replaceDict = require('./replaceDict');
 
+// If certain file is not wanted to be processed in data-folder,
+// it should be in this list.
+const excluded = [
+    'examplefile.json'
+];
+
+
 /**
  * Reads contents of files in certain directory and returns
- * promise.
+ * a promise.
  */
 const readFiles = (dir) => {
     return new Promise((resolve, reject) => {
         fs.readdir(dir, (e, files) => e ? reject(e) : resolve(files));
-    })
-    .then(files => Promise.all(files
-        .filter(name => name.substr(-5) === '.json')
+    }).then(files => Promise.all(files
+        .filter(name => name.substr(-5) === '.json' && !excluded.includes(name))
         .map(name => new Promise((resolve, reject) => {
             fs.readFile(dir + name, 'utf-8', (e, data) => e ? reject(e) : resolve({name, data}));
         }))
@@ -27,18 +33,17 @@ readFiles('./src/data/')
         response.map(file => {
             const regExp = new RegExp(Object.keys(replaceDict).join('|'), 'ig')
             const newContent = file.data.replace(regExp, match => {
-                console.log(`Replaced: ${match} with ${replaceDict[match]} in ${file.name}`);
+                console.log(`Replaced: "${match}" with "${replaceDict[match]}" in ${file.name}`);
                 return replaceDict[match];
             });
-            if (newContent !== file.data) {
+            if (newContent !== file.data) { // <- quite heavy if expression
                 fs.writeFile(`./src/data/${file.name}`, newContent, e => {
                     if (e) throw e;
                     console.log(`File ${file.name} saved!`);
                 });
             }
         });
-    })
-    .catch(e => console.log(e));
+    }).catch(e => console.log(e));
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,57 @@
 const path = require('path');
+const webpack = require('webpack');
 const copyPlugin = require('copy-webpack-plugin');
 const htmlPlugin = require('html-webpack-plugin');
 
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  cache: false,
-  output:{
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query:{
-          presets: ['env', 'stage-2']
+module.exports = env => {
+  return {
+    entry: ['babel-polyfill', './src/index.js'],
+    cache: false,
+    output:{
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query:{
+            presets: [
+              ['env', {
+                'modules': false,
+                'targets': {'browsers': ['last 2 versions']}
+              }],
+              'stage-2']
+          }
         }
-      }
-    ]
-  },
-  plugins: [
-    new copyPlugin([
-      {from: 'src/data', to: 'data'}
-    ]),
-    new htmlPlugin({
-      template: './src/index.ejs',
-      fileName: 'index.html',
-      inject: false
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    publicPath: '/',
-    compress: true,
-    port: 8080,
-    historyApiFallback: true,
-    inline: true
-  }
+      ]
+    },
+    plugins: [
+      new copyPlugin([
+        {from: 'src/data', to: 'data'}
+      ]),
+      new htmlPlugin({
+        template: './src/index.ejs',
+        fileName: 'index.html',
+        inject: false
+      }),
+      ...(env.prod ? [
+        new webpack.optimize.UglifyJsPlugin({
+          comments: false
+        })
+      ] : [])
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      publicPath: '/',
+      compress: true,
+      port: 8080,
+      historyApiFallback: true,
+      inline: true
+    }
+  };
 };
