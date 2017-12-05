ed3bcb35c26e26efaf525eefe1db58bec39af550
TheHenkka <hentter.eloranta@gmail.com>
2017-12-05 13:00:35 +0200

    First draft of animation and some changes to map CSS. Also added function to get number of refugees per country

--- src/components/map/util/map.js
+++ src/components/map/util/map.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import slugify from 'slugify';
 import { svg } from '../map-settings';
 import { additionalCountryAdded } from '../events';
+import { transition } from 'd3-transition';
 
 
 /**
@@ -12,10 +13,10 @@ import { additionalCountryAdded } from '../events';
  */
 export const drawArcs = c => {
     const bend = 1.3;
-    const d = {source: c[0], target: c[1]};
+    const d = { source: c[0], target: c[1] };
     const dx = d.target[0] - d.source[0];
     const dy = d.target[1] - d.source[1];
-    const dr = Math.sqrt(dx * dx + dy * dy)*bend;
+    const dr = Math.sqrt(dx * dx + dy * dy) * bend;
     const sweep = d.target[0] - d.source[0] < 0 ? 0 : 1;
     return `M${d.source[0]},${d.source[1]}A${dr} ${dr} 0 0,${sweep} ${d.target[0]},${d.target[1]}`;
 };
@@ -33,7 +34,7 @@ export const processCoordinates = (traffic) => {
 
     if (traffic[window.year]) {
         traffic[window.year].map((countryObject) => {
-            const {country} = countryObject;
+            const { country } = countryObject;
             if (!country
                 || country === 'Various/Unknown'
                 || country === 'Stateless') return true;
@@ -43,7 +44,7 @@ export const processCoordinates = (traffic) => {
             // those countries from window.additionalCountries
             if (!isValidCountry(country)) {
                 const isNew = window.additionalCountries.to != window.country
-                    ||  window.additionalCountries.year != window.year; 
+                    || window.additionalCountries.year != window.year;
                 if (isNew) {
                     window.additionalCountries.to = window.country;
                     window.additionalCountries.year = window.year;
@@ -63,21 +64,59 @@ export const processCoordinates = (traffic) => {
             ];
             window.map.fromCountryList.push(country);
             window.map.allCoordinates.push(coordinates);
+
+            var totalRefugees = 0;
+            var refugeeCount = 0;
+            var asylumCount = 0;
+
+            //Get refugee count per country so it can be used to create function in animation
+            //TODO: Better way to get the data??
+            for (let i in traffic[window.year]) {
+
+                if (traffic[window.year][i].country == country) {
+
+                    if (typeof traffic[window.year][i].countRefugee != 'undefined') {
+                        refugeeCount = parseInt(traffic[window.year][i].countRefugee);
+                    }
+
+                    if (typeof traffic[window.year][i].countAsylum != 'undefined') {
+                        asylumCount = parseInt(traffic[window.year][i].countAsylum);
+                    }
+
+                    totalRefugees = refugeeCount + asylumCount;
+                    //console.log("kk", totalRefugees);
+                }
+            }
+
             // this is unnessessary... here just for demo purpose
             svg.append('path')
                 .datum(coordinates)
                 .attr('d', drawArcs)
                 .attr('class', 'arc')
-                .exit();
+                //.attr("stroke", 'blue');
+            //.exit();
+
+            //Just some playing with animations. Changing "stroke-dashoffset" and/or "stroke-dasharray"
+            //based on refugee count could show traffic amount. Or changing "stroke"
+            //TODO: Better animation
+            selectAll('.arc')
+                .attr("stroke-dasharray", 100)
+                .attr("stroke-dashoffset", 100)
+                //.attr("stroke", 'blue')
+                .transition()
+                //.duration(3000)
+                .attr("stroke-dashoffset", 0);
+                //.attr("stroke", 'red');
+                //.exit();
         });
     }
 };
 
 export const getRefugeeData = async () => {
-    const slugOptions = {lower: true, remove: /[$*_+~.,()'"!\-:@]/g};
+    const slugOptions = { lower: true, remove: /[$*_+~.,()'"!\-:@]/g };
     const dataResponse = await axios.get(`/data/${slugify(window.country, slugOptions)}.json`);
     if (dataResponse.status === 200) {
-        return dataResponse.data; 
+        return dataResponse.data;
     }
 };
 
