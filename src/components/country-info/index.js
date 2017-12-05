d86fb54ccbe276f03afb5eeb2d5f48722859fe25
tajomyll <tajomyll@student.jyu.fi>
2017-12-05 15:26:57 +0200

    Sivupalkin tiedot tulostuvat

--- src/components/country-info/index.js
+++ src/components/country-info/index.js
@@ -24,17 +24,24 @@ export const printCountryData = () => {
     countryInfo.getElementsByClassName('side-title')[0].innerHTML = country;
 
     var yearGDP = R.find(R.propEq('Country Name', window.country), gdp)[year];
-    //var yearCapita = R.find(R.propEq('Country Name', window.country), gdpcapita)[year];    
-    // var yearLE = R.find(R.propEq('Country Name', window.country), le)[year];
-    // var yearPopulation = R.find(R.propEq('Country Name', window.country), population)[year];
+    var yearCapita = R.find(R.propEq('Country Name', window.country), gdpcapita)[year];    
+    var yearLE = R.find(R.propEq('Country Name', window.country), le)[year];
+    var yearPopulation = R.find(R.propEq('Country Name', window.country), population)[year];
     
     console.log('GDP: ' + numberInUnits(parseFloat(yearGDP.replace(',', '.')), 9, 2) + ' billion; Country: ' + window.country );
-    //console.log("Capita: " + yearCapita + "; Country: " + window.country );
-    // console.log("Life Expectancy: " + yearLE + "; Country: " + window.country );
-    // console.log("Population: " + yearPopulation + "; Country: " + window.country );
+    console.log("Capita: " + numberInUnits(parseFloat(yearCapita.replace(',', '.')), 3, 2) + " k; Country: " + window.country );
+    console.log("Life Expectancy: " + numberInUnits(parseFloat(yearLE.replace(',', '.')), 0, 1) + " years; Country: " + window.country );
+    console.log("Population: " + numberInUnits(parseFloat(yearPopulation), 6, 2) + " million; Country: " + window.country );
 
-    //var gdpEl = document.getElementById('gdp');
-    //gdpEl.innerHTML = 'GDP: ' + numberInUnits(parseFloat(yearGDP.replace(',', '.')), 9, 2) + ' bil.';
+    var gdpEl = document.getElementById('gdp');
+	var gdpCapitaEl = document.getElementById('gdp-capita');
+	var lifeExpectancyEl = document.getElementById('life-expentancy');
+	var populationEl = document.getElementById('population');
+    gdpEl.innerHTML = 'GDP: ' + numberInUnits(parseFloat(yearGDP.replace(',', '.')), 9, 2) + ' bil.';
+	gdpCapitaEl.innerHTML = 'GDP (capita): ' + numberInUnits(parseFloat(yearCapita.replace(',', '.')), 3, 2) + 'k';
+	lifeExpectancyEl.innerHTML = 'Life expectancy: ' + numberInUnits(parseFloat(yearLE.replace(',', '.')), 0, 1) + ' years';
+	populationEl.innerHTML = 'Population: ' + numberInUnits(parseFloat(yearPopulation), 6, 3) + ' mil.';
+	
     //let gdptest = Object.keys(population).map((key) => {
     //  const countryCodeObj = {'Country Name': key};
     //  const row = {...countryCodeObj, ...population[key]};
