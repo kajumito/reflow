13aa17e3ba27b4e6e1461526293ca7c1ce35f732
Kasper Tontti <kaspertontti@Kaspers-MacBook-Pro.local>
2017-12-13 20:05:16 +0200

    maat, jotka eivät näy kartalla

--- src/components/country-info/listeners.js
+++ src/components/country-info/listeners.js
@@ -17,4 +17,31 @@ export default () => {
         printCountryData();
     }, false);
 
+    window.addEventListener('additionalCountryAdded', () => {
+
+        var unlistedCountries = document.getElementById('unlisted-countries');
+        var unlistedAmount = document.getElementById('unlisted-amount');
+
+        while (unlistedCountries.firstChild && unlistedAmount.firstChild) {
+            unlistedCountries.removeChild(unlistedCountries.firstChild);
+            unlistedAmount.removeChild(unlistedAmount.firstChild);
+        }
+
+        for(var i = 0; i < additionalCountries.countries.length; i++) {
+            if(additionalCountries.countries[i].countRefugee != undefined
+                && additionalCountries.countries[i].countRefugee != 0){
+                var unlistedCountry = document.createElement('li');
+                var refugeeAmount = document.createElement('li');
+                unlistedCountry.classList.add('unlisted-country-name');
+                refugeeAmount.classList.add('unlisted-country-amount');
+                var countryName = document.createTextNode(additionalCountries.countries[i].country);
+                var amount = document.createTextNode(additionalCountries.countries[i].countRefugee);
+                unlistedCountry.appendChild(countryName);
+                refugeeAmount.appendChild(amount);
+                unlistedCountries.appendChild(unlistedCountry);
+                unlistedAmount.appendChild(refugeeAmount);
+            }
+        }
+    }, false);
+
 };
