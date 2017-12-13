053c4079c1c385ba6b7dc2a0c5862ab94dddb75f
TheHenkka <32512570+TheHenkka@users.noreply.github.com>
2017-12-13 18:17:16 +0200

    Searching for the country is now easier and country drop down list is OK.

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -20,88 +20,76 @@ import { countryChanged } from './events';
 
 let groupCountries = svg.append('g');
 let groupCentroids = svg.append('g');
+let worldCountryList = [];
 export let allCoordinates = [];
-export let fromCountryList=[];
+export let fromCountryList = [];
 
 
-//TODO: Get real country list 
+
+//Lists all the countries in the world to a drop down list so user can search for the country without clicking the map
 function makeDropDownList() {
 
     var dropDownList = document.getElementById("dropDownList");
 
-    var k = document.createElement("option");
-    k.setAttribute("value", "Sweden");
-    k.appendChild(document.createTextNode("Sweden"));
+    for (let i in worldCountryList) {
+        var listItem = document.createElement("option");
+        listItem.setAttribute("value", worldCountryList[i]);
+        listItem.appendChild(document.createTextNode(worldCountryList[i]));
+        dropDownList.appendChild(listItem);
+    }
 
-    var k1 = document.createElement("option");
-    k1.setAttribute("value", "Finland");
-    k1.appendChild(document.createTextNode("Finland"));
-
-    var k2 = document.createElement("option");
-    k2.setAttribute("value", "Germany");
-    k2.appendChild(document.createTextNode("Germany"));
-
-    var k3 = document.createElement("option");
-    k3.setAttribute("value", "Slovenia");
-    k3.appendChild(document.createTextNode("Slovenia"));
-
-    dropDownList.appendChild(k);
-    dropDownList.appendChild(k1);
-    dropDownList.appendChild(k2);
-    dropDownList.appendChild(k3);
-
-    var dropDown = document.getElementById("dropDownDiv");
-
-    var but = document.createElement("button");
-    
-    but.innerHTML = "Do Something";
-    dropDown.appendChild(but);
-    but.addEventListener("click",function(){
-
-        var a = document.getElementById("dropDownList").value;
-        window.country = a;
+    document.getElementById("countryBut").addEventListener("click", function () {
+        var countryFromList = document.getElementById("dropDownList").value;
+        window.country = countryFromList;
         window.dispatchEvent(countryChanged);
+    });
+
+    document.getElementById("search").addEventListener("click", function () {
+        document.getElementById("dropDownList").classList.toggle("show");
+        document.getElementById("countryBut").classList.toggle("show");
     });
 }
 
 //Creates a tooltip for seeing how many refugees came from highlighted country to 
 //currently selected country.
-function makeTooltip()
-{
-	
-	//Creating the actual tooltip element
-	var tooltip = d3select.select("body")
-	.append("div")
-	.attr("class", "tooltip")
-	.style("position", "absolute")
-	.style("text-align", "center")
-	.style("background", "#F0F0F0")
-	.style("border", "0px")
-	.style("border-radius", "8px")
-	.style("padding", "2px")
-	.style("pointer-events", "none")
-	
-	d3select.selectAll("path")
-		.on("mouseover", async function(d) { 
-			var refugeeData = await getRefugeeData();
-			tooltip.style("visibility", "visible");
-			var refugeeAmount;
-			//If there are no refugees from highlighted country, then the amount is set to zero.
-			try {
-				
-				refugeeAmount = R.find(R.propEq("country", d.properties.NAME), refugeeData[window.year])["countRefugee"];
-				
-			} catch (error) {
-				
-				refugeeAmount = 0;
-				
-			}
-			
-			tooltip.text(d.properties.NAME + ": " + refugeeAmount);})
-		.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px")
-			.style("left",(event.pageX+10)+"px");})
-		.on("mouseout", function() { return tooltip.style("visibility", "hidden"); });
-		
+function makeTooltip() {
+
+    //Creating the actual tooltip element
+    var tooltip = d3select.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("text-align", "center")
+        .style("background", "#F0F0F0")
+        .style("border", "0px")
+        .style("border-radius", "8px")
+        .style("padding", "2px")
+        .style("pointer-events", "none")
+
+    d3select.selectAll("path")
+        .on("mouseover", async function (d) {
+            var refugeeData = await getRefugeeData();
+            tooltip.style("visibility", "visible");
+            var refugeeAmount;
+            //If there are no refugees from highlighted country, then the amount is set to zero.
+            try {
+
+                refugeeAmount = R.find(R.propEq("country", d.properties.NAME), refugeeData[window.year])["countRefugee"];
+
+            } catch (error) {
+
+                refugeeAmount = 0;
+
+            }
+
+            tooltip.text(d.properties.NAME + ": " + refugeeAmount);
+        })
+        .on("mousemove", function () {
+            return tooltip.style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
+
 }
 
 const drawMap = (countries, traffic) => {
@@ -110,6 +98,7 @@ const drawMap = (countries, traffic) => {
     // append centroids for each country
     window.map.geoData.map(d => {
         d.centroid = projection(geoCentroid(d));
+        worldCountryList.push(d.properties.NAME);
     });
 
     // draw countries
@@ -121,7 +110,7 @@ const drawMap = (countries, traffic) => {
         .attr('id', (d) => d.properties.NAME)
         .attr('country-code', (d) => d.properties.ADM0_A3)
         .attr('d', path)
-		.exit();        
+        .exit();
 
     // draw centroids of countries
     //groupCentroids.attr('class', 'centroids')
@@ -138,13 +127,13 @@ const drawMap = (countries, traffic) => {
     // Save current year's traffic coordinates to a global variable
     processCoordinates(traffic);
 
-//todo: animation intensity should depend on traffic amount.
-//let i = 0;
-//setInterval(_ => {
-//  if (i > window.map.allCoordinates.length - 1) i = 0;
-//  moveItemAlongPath(window.map.allCoordinates[i], svg);
-//  i++;
-//  }, 300);
+    //todo: animation intensity should depend on traffic amount.
+    //let i = 0;
+    //setInterval(_ => {
+    //  if (i > window.map.allCoordinates.length - 1) i = 0;
+    //  moveItemAlongPath(window.map.allCoordinates[i], svg);
+    //  i++;
+    //  }, 300);
 };
 
 export default async () => {
@@ -156,12 +145,12 @@ export default async () => {
     }
     listeners();
     makeDropDownList();
-	makeTooltip();
+    makeTooltip();
 
     // Init target-country class from window.country
     const countriesEl = document.querySelector('.countries');
     countriesEl.childNodes.forEach((el) => {
-        if(el.id === window.country) el.classList.add('target-country');
+        if (el.id === window.country) el.classList.add('target-country');
     });
 
     // Init selected country from localStorage if set
